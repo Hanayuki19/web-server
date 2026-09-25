167e7c9ed44557fa5cbb5dd2956a251927115605
Hanayuki19 <minble@umass.edu>
2026-09-25 13:09:17 +0000

    Add .gitignore

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 app.set("view engine", "ejs");
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, web!');
+app.get("/", (req, res) => {
+  res.send("Hello, web!");
 });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, web!');
+app.get("/hello", (req, res) => {
+  res.send("Hello, web!");
 });
 
-app.get('/hello/:name', (req, res) => {
+app.get("/hello/:name", (req, res) => {
   res.send(`Hello, ${req.params.name}!`);
 });
 
-app.get('/repeat/:word', (req, res) => {
+app.get("/repeat/:word", (req, res) => {
   res.send(`${req.params.word}`.repeat(3));
 });
 
-app.get('/count', (req, res) => {
-  const from = req.query.from || '1';
-  const to = req.query.to || '10';
+app.get("/count", (req, res) => {
+  const from = req.query.from || "1";
+  const to = req.query.to || "10";
   res.send(`Counting from ${from} to ${to}.`);
 });
 
@@ -30,12 +30,12 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get('/api/info', (req, res) => {
-  res.json({ course: 'COMPSCI 326', topic: 'Web Programming'});
+app.get("/api/info", (req, res) => {
+  res.json({ course: "COMPSCI 326", topic: "Web Programming" });
 });
 
-app.get('/api/error', (req, res) => {
-  res.status(400).send('Bad request.');
+app.get("/api/error", (req, res) => {
+  res.status(400).send("Bad request.");
 });
 
 app.listen(PORT, () => {
